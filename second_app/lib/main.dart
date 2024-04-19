41e81643653183c47eb17b8d88c20d2e58a95955
Ogulcan Oktem <ogulcanoktem@gmail.com>
2024-04-19 10:52:36 +0300

    ikinci projenin ilk dosyaları oluşturuldu.

---
+++ second_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
