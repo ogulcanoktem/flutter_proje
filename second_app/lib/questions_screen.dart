99e7e4f1492e5c0ec72e98f4069515f811fedab3
Ogulcan Oktem <ogulcanoktem@gmail.com>
2024-04-22 13:41:40 +0300

    cevapları random karıştıran fonksiyon eklendi.

--- second_app/lib/questions_screen.dart
+++ second_app/lib/questions_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/answer_button.dart';
+import 'package:second_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
@@ -10,6 +14,32 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions Screen!');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) {
+                return AnswerButton(answerText: answer, onTop: () {});
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
