87884e8169ce00ec6d0c5a582022ff66cfd51331
Ogulcan Oktem <ogulcanoktem@gmail.com>
2024-04-26 15:39:08 +0300

    ekleme sayfasına date eklendi.

--- third_app/lib/widgets/expenses.dart
+++ third_app/lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:third_app/widgets/expenses_list/expenses_list.dart';
 import 'package:third_app/models/expense.dart';
+import 'package:third_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -30,7 +31,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text('Modal bottom sheet'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
