3c99de909f20b922f45f7e7678677c9762abbed8
Ogulcan Oktem <ogulcanoktem@gmail.com>
2024-04-30 15:58:22 +0300

    yatay kullanılabilir yapıldı uygulama.

--- third_app/lib/main.dart
+++ third_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 import 'package:third_app/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
@@ -11,6 +12,11 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then(
+  //   (fn) {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -53,4 +59,6 @@ void main() {
       home: const Expenses(),
     ),
   );
+//    },
+//  );
 }
