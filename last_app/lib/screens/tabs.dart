878a19e01eeebca3f5fc064954ce46b4ed39a05b
Ogulcan Oktem <ogulcanoktem@gmail.com>
2024-05-09 10:33:30 +0300

    favori ekleme iconun eklendiğinde dolu çıkarıldığında boş olmasını ayarladım.

--- last_app/lib/screens/tabs.dart
+++ last_app/lib/screens/tabs.dart
@@ -4,7 +4,6 @@ import 'package:last_app/screens/categories.dart';
 import 'package:last_app/screens/filters.dart';
 import 'package:last_app/screens/meals.dart';
 import 'package:last_app/widgets/main_drawer.dart';
-import 'package:last_app/providers/meals_provider.dart';
 import 'package:last_app/providers/favorites_provider.dart';
 import 'package:last_app/providers/filters_provider.dart';
 
@@ -46,23 +45,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
